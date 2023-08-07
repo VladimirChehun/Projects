eb70bf8f2e7cb5a2aca0bbc1d2965c372653f6ce
VladimirChehun <bazweb@skarb.com.ua>
2023-08-07 10:32:17 +0300

    11

--- Test-Python/main.py
+++ Test-Python/main.py
@@ -1,2 +1,6 @@
-print('Hello')
-print('Test')
+name = input('What is you name?')
+age = input('How old are you?')
+if int(age) >= 18:
+    print (f'{name}, you are aldut already')
+else:
+    print (f'{name}, you are infant yet')
